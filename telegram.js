01cc93b91db938cf96e5a5fbbb683fcc3dde5b9e
Tom Counsell <contact@tomcounsell.com>
2017-10-18 06:03:06 +0200

    api example

--- telegram.js
+++ telegram.js
@@ -1,6 +1,8 @@
+const request = require('request');
 const express = require('express');
 const app = express();
 const TelegramBot = require('node-telegram-bot-api');
+
 
 // const aws = require('aws-sdk');
 
@@ -14,6 +16,7 @@ const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
 const token = process.env.TELEGRAM_BOT_TOKEN;
+const api_host_url = process.env.ITT_API_HOST;
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
@@ -29,6 +32,27 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 
   // send back the matched "whatever" to the chat
   bot.sendMessage(chatId, resp);
+});
+
+bot.onText(/\/price (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const coin = match[1]; // the captured "whatever"
+
+  var resp = ""
+
+  request(api_host_url+'/price', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var info = JSON.parse(body)
+        if ('price' in info){
+          bot.sendMessage(chatId, (parseFloat(info['price']/100000000)).toString() + " BTC")
+        } else {
+          bot.sendMessage(chatId, "coin not found")
+        }
+      }
+  })
+
+  // send back the matched "whatever" to the chat
+
 });
 
 // Listen for any kind of message. There are different kinds of
