74da4be69719bb801316dacbd9456bbd1b57c7b3
delisif <fr.delisi@gmail.com>
2017-10-18 18:47:09 +0630

    [FIX] Command rejection answer and parameter check

--- commands/price.js
+++ commands/price.js
@@ -2,21 +2,27 @@ const request = require('request');
 
 var price = {
     getPrice: (coin) => new Promise((resolve, reject) => {
-        var uri = `https://${process.env.ITT_API_HOST}/price?coin=${coin}`;
 
-        request(uri, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                var info = JSON.parse(body)
-                if ('price' in info) {
-                    resolve(parseFloat(info['price'] / 100000000));
-                } else {
-                    reject('Coin not found');
+        if (coin === "") {
+            reject("Write `/price <ticker>` to get the latest price.\nFor example: /price ETH")
+        } else {
+
+            var uri = `https://${process.env.ITT_API_HOST}/price?coin=${coin}`;
+
+            request(uri, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    var info = JSON.parse(body)
+                    if ('price' in info) {
+                        resolve(parseFloat(info['price'] / 100000000));
+                    } else {
+                        reject('Coin not found');
+                    }
                 }
-            }
-            else {
-                reject('Uh-Oh, something went wrong! Please retry...');
-            }
-        });
+                else {
+                    reject('Uh-Oh, something went wrong! Please retry...');
+                }
+            });
+        }
     })
 }
 
--- commands/volume.js
+++ commands/volume.js
@@ -2,23 +2,28 @@ const request = require('request');
 
 var volume = {
     getVolume: (coin) => new Promise((resolve, reject) => {
-        var uri = `https://${process.env.ITT_API_HOST}/volume?coin=${coin}`;
 
-        request(uri, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                var info = JSON.parse(body)
-                if ('volume' in info) {
-                    resolve(parseFloat(info['volume'] / 100000000));
-                } else {
-                    reject('Coin not found');
+        if (coin === "") {
+            reject("Write `/volume <ticker>` to get the volume.\nFor example: /volume ETH")
+        } else {
+
+            var uri = `https://${process.env.ITT_API_HOST}/volume?coin=${coin}`;
+
+            request(uri, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    var info = JSON.parse(body)
+                    if ('volume' in info) {
+                        resolve(parseFloat(info['volume'] / 100000000));
+                    } else {
+                        reject('Coin not found');
+                    }
                 }
-            }
-            else {
-                reject('Uh-Oh, something went wrong! Please retry...');
-            }
-        });
+                else {
+                    reject('Uh-Oh, something went wrong! Please retry...');
+                }
+            });
+        }
     })
 }
-
 
 exports.volume = volume;
