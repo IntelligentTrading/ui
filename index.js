576e638855d52efb69f7041926607bcf0961dc71
delisif <fr.delisi@gmail.com>
2017-10-24 19:08:08 +0630

    Chart
    Test support for dynamic charts

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -6,6 +7,10 @@ app.set('port', (process.env.PORT || 5000));
 app.get('/', function(request, response) {
   // response.render('pages/index');
   response.sendStatus(200)
+});
+
+app.get('/test', function(request,response){
+  response.sendFile(path.join(__dirname,'pages','/test.html'));
 });
 
 app.listen(app.get('port'), function() {
