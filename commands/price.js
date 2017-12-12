6e1ccdec5a7ddab6e37bb9f37508e9015b8dbbc7
Brbb <franxster@gmail.com>
2017-12-12 15:17:30 +0630

    Price fix

--- commands/price.js
+++ commands/price.js
@@ -11,7 +11,7 @@ var price = {
         return api.price(currency)
             .then(result => {
                 var info = JSON.parse(result)
-                if ('price_satoshis' in info) {
+                if ('price' in info) {
                     return parse_info(info);
                 } else {
                     return 'Currency not found';
@@ -42,20 +42,20 @@ function parse_info(info_data) {
             var retrieved_price;
             var price_change_sign;
 
-            if (info_data.coin == 'BTC') {
-                retrieved_price = `${parseFloat(info_data['price_usdt']).toFixed(2)} USD`;
+            if (info_data.base_coin == 0) {
+                retrieved_price = `${parseFloat(info_data['price']/100000000).toFixed(6)} BTC`;
             }
             else
-                retrieved_price = `${parseFloat(info_data['price_satoshis'] / 100000000).toFixed(4)} BTC`;
+                retrieved_price = `${parseFloat(info_data['price']).toFixed(2)} USD`; //!It might be not correct
 
-            if (info_data.price_satoshis_change > 0)
+            if (info_data.price_change > 0)
                 price_change_sign = '↗️';
-            else if (info_data.price_satoshis_change < 0)
+            else if (info_data.price_change < 0)
                 price_change_sign = '↘️';
             else
                 price_change_sign = '➡️';
 
-            return `${currency_wiki}\nPrice: ${retrieved_price} (${(info_data.price_satoshis_change * 100).toFixed(2)}% ${price_change_sign})\nLast update: ${info_data.timestamp.split('.')[0]}\nSource: ${info_data.source.toSentenceCase()}`;
+            return `${currency_wiki}\nPrice: ${retrieved_price} (${(info_data.price_change * 100).toFixed(2)}% ${price_change_sign})\nLast update: ${info_data.timestamp.split('.')[0]}\nSource: ${info_data.source.toSentenceCase()}`;
         })
         .catch(reason => errorManager.handleException(reason));
 }
