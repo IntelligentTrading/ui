34dfc23cca6d2b94eefc53993bcf4680e70a1773
Brbb <franxster@gmail.com>
2017-12-13 23:42:55 +0630

    Fixed telegram instant view hash

--- commands/start.js
+++ commands/start.js
@@ -1,9 +1,10 @@
 var help = require('./help').help;
 
-var eula_endpoint = process.env.ITT_NODE_SERVICES.split('://')[1]
+var eula_endpoint = process.env.ITT_NODE_SERVICES.replace('beta','prod').split('://')[1]
+var rhash = eula_endpoint.indexOf('prod') >= 0 || eula_endpoint.indexOf('beta') >=0 ? '47f5ff678046c3' : '69471435748184';
 
 var start = {
-    eula_text: (chat_id) => `Hi! I'm the ITT Trading Bot. Before you activate your account you MUST accept the [EULA](https://t.me/iv?url=https%3A%2F%2F${eula_endpoint}%2Feula%3Fu%3D${chat_id}&rhash=69471435748184)`
+    eula_text: (chat_id) => `Hi! I'm the ITT Trading Bot. Before you activate your account you MUST accept the [EULA](https://t.me/iv?url=https%3A%2F%2F${eula_endpoint}%2Feula%3Fu%3D${chat_id}&rhash=${rhash})`
 }
 
 exports.start = start;
