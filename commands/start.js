6e7160e6d9745416ce3ecd6821ee3fa66094e94e
Brbb <franxster@gmail.com>
2017-12-13 23:49:26 +0630

    Fix hash telegram
    (hardcoded, think about a better fix)

--- commands/start.js
+++ commands/start.js
@@ -1,7 +1,19 @@
 var help = require('./help').help;
 
-var eula_endpoint = process.env.ITT_NODE_SERVICES.replace('beta','prod').split('://')[1]
-var rhash = eula_endpoint.indexOf('prod') >= 0 || eula_endpoint.indexOf('beta') >=0 ? '47f5ff678046c3' : '69471435748184';
+var eula_endpoint = process.env.ITT_NODE_SERVICES.split('://')[1]
+var rhash;
+
+if (eula_endpoint.indexOf('prod') >= 0) {
+    rhash = '47f5ff678046c3';
+}
+else if (eula_endpoint.indexOf('beta') >= 0) {
+    rhash = '38e694f259517c';
+}
+else {
+    rhash = '69471435748184'
+}
+
+
 
 var start = {
     eula_text: (chat_id) => `Hi! I'm the ITT Trading Bot. Before you activate your account you MUST accept the [EULA](https://t.me/iv?url=https%3A%2F%2F${eula_endpoint}%2Feula%3Fu%3D${chat_id}&rhash=${rhash})`
