2b65d0b37c0056f2a7e1551e6c80c71748ba16f2
Brbb <franxster@gmail.com>
2017-12-13 23:18:24 +0630

    Fixed eula endpoint

--- commands/start.js
+++ commands/start.js
@@ -1,7 +1,9 @@
 var help = require('./help').help;
 
+var eula_endpoint = process.env.ITT_NODE_SERVICES.split('://')[1]
+
 var start = {
-    eula_text: (chat_id) => `Hi! I'm the ITT Trading Bot. Before you activate your account you MUST accept the [EULA](https://t.me/iv?url=https%3A%2F%2Fitt-node-services.herokuapp.com%2Feula%3Fu%3D${chat_id}&rhash=69471435748184)`
+    eula_text: (chat_id) => `Hi! I'm the ITT Trading Bot. Before you activate your account you MUST accept the [EULA](https://t.me/iv?url=https%3A%2F%2F${eula_endpoint}%2Feula%3Fu%3D${chat_id}&rhash=69471435748184)`
 }
 
 exports.start = start;
