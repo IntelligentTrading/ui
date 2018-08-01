13e20ad384973921ec33e6daf9b1a51893004425
Francesco De Lisi <francesco@intelligenttrading.org>
2018-08-01 18:49:51 +0700

    [Feature] Referral code
    https://app.asana.com/0/567041585915013/756441058622670/f

--- core/api.js
+++ core/api.js
@@ -188,6 +188,22 @@ var api = {
         }
 
         return rpromise(request_opts)
+    },
+    referral: (telegram_chat_id, code) => {
+        var request_opts = {
+            method: 'POST',
+            body: {
+                telegram_chat_id: telegram_chat_id,
+                code: code
+            },
+            json: true
+        }
+        request_opts.url = `${node_svc_api}/users/referral`
+        request_opts.headers = {
+            'NSVC-API-KEY': node_svc_api_key
+        }
+
+        return rpromise(request_opts)
     }
 }
 
