fcf224d206b9b8f4ddae01067ed8248f100acd4e
Francesco De Lisi <francesco@intelligenttrading.org>
2018-10-27 15:25:49 +0700

    BOT alignement to new plans

--- commands/controllers/subscribe.js
+++ commands/controllers/subscribe.js
@@ -4,20 +4,18 @@ var dateUtils = require('../../util/dates')
 var nopreview_markdown_opts = require('../../bot/telegramInstance').nopreview_markdown_opts
 
 var moduleBot = null
-var upgradeToStarter = 'Upgrade to the Starter plan by sending'
+var upgradeToPro = 'Upgrade to the Pro plan by sending'
 var extendYourSubscription = (daysLeft) => { return `Your subscription will expire in ${Math.max(daysLeft, 0)} days.\nTo extend your subscription send` }
 
 var getCurrentStatusMessage = (settings, itt_usd_rate) => {
-    var betaDaysLeft = dateUtils.getDaysLeftFrom(settings.subscriptions.beta)
     var paidDaysLeft = dateUtils.getDaysLeftFrom(settings.subscriptions.paid)
-    var currentPlan = paidDaysLeft > 0 || settings.is_ITT_team ? 'Starter' : (betaDaysLeft > 0 ? 'FREE+' : 'FREE')
-    var stakeholderStatus = settings.staking ? (settings.staking.centomila || settings.is_ITT_team ? 'Advanced' : (settings.staking.diecimila ? 'Pro' : '')) : ''
+    var currentPlan = paidDaysLeft > 0 || settings.is_ITT_team || settings.staking.diecimila ? 'Pro' : 'FREE'
     var usdPricePerSecond = 20 * 12 / 365.25 / 24 / 3600
     var oneMonthInSeconds = 2629746
 
-    return `Subscription | *${stakeholderStatus != '' ? stakeholderStatus : currentPlan}* plan\n[View all available subscription plans](intelligenttrading.org/pricing).
+    return `Subscription | *${currentPlan}* plan\n[View all available subscription plans](intelligenttrading.org/pricing).
 
-${paidDaysLeft <= 0 && !settings.is_ITT_team ? upgradeToStarter : extendYourSubscription(paidDaysLeft)} ITT tokens to the address below. You will be notified as soon as the transaction is confirmed.
+${paidDaysLeft <= 0 && !settings.is_ITT_team ? upgradeToPro : extendYourSubscription(paidDaysLeft)} ITT tokens to the address below. You will be notified as soon as the transaction is confirmed.
 
 ‣ Receiver address: ${settings.ittWalletReceiverAddress}
 ‣ ITT token price: $${itt_usd_rate} USDT
